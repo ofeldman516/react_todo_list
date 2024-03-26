5a48330fedf4b92cb83a53e9d43c79d1401499d0
Olivia Feldman <orwfeldman@gmail.com>
2024-03-26 14:16:55 +0100

    Added uuid, useEffect and setValue to reset the search bar

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,16 @@
 import React, {useState} from 'react'
 
-export const TodoForm = () => {
+export const TodoForm = ({addTodo}) => {
   const [value, setValue] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    addTodo(value);
+    setValue('');
+
+  }
   return (
-    <form className='TodoForm'>
-      <input type='text' className='todo-input' placeholder="Today's Task" />
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input type='text' className='todo-input'  value={value} placeholder="Today's Task" onChange={(e) => setValue(e.target.value)}/>
       <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import { TodoForm } from './TodoForm'
+import React, { useState, useEffect } from 'react';
+import { TodoForm } from './TodoForm';
+import { v4 as uuidv4 } from 'uuid';
+uuidv4();
 
 export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = todo => {
+    setTodos([...todos, { id: uuidv4(), task: todo, completed: false, isEditing: false }]);
+  }
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
   return (
     <div className='todo-wrapper'>
-      <TodoForm />
+      <TodoForm addTodo={addTodo} />
     </div>
   )
 }
