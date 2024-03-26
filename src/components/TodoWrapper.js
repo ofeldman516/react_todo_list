02adc9ecb81907e9bb0db057fa4b2285752ead7f
Olivia Feldman <orwfeldman@gmail.com>
2024-03-26 17:36:10 +0100

    Added EditToDoForm component

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { TodoForm } from './TodoForm.js';
 import {Todo} from './Todo'
 import { v4 as uuidv4 } from 'uuid';
+import { EditTodoForm } from './EditToDoForm.js';
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -22,14 +23,27 @@ export const TodoWrapper = () => {
   const deleteTodo = id => {
     setTodos(todos.filter(todo => todo.id !== id))
   }
+
+  const editTodo = id => {
+    setTodos(todos.map(todo => todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo))
+  }
+
+  const editTask = (task, id) => {
+    setTodos(todos.map(todo => todo.id === id ? {...todo, task, isEditing: !todo.isEditing} : todo))
+  }
   return (
     <div className='todo-wrapper'>
       <h1>My To Do List</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) => (
-        <Todo task={todo} key={index}
-        toggleComplete={toggleComplete}
-        deleteTodo = {deleteTodo} />
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+
+          <Todo task={todo} key={index}
+          toggleComplete={toggleComplete}
+          deleteTodo = {deleteTodo} editTodo={editTodo}/>
+        )
       ))}
     </div>
   )
